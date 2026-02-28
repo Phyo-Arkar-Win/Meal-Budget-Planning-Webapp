220854493b1b95abf44fc7051186104c0f118014
Phyo Arkar Win <phyoarkar3730@gmail.com>
2026-02-28 14:48:37 +0700

    Added Draft Food Database

--- server/src/models/food.ts
+++ server/src/models/food.ts
@@ -1,27 +1,31 @@
 import mongoose, { Document, Schema } from 'mongoose';
 
-// TypeScript interface for Food document
-export interface IFood extends Document {
+export interface IUser extends Document{
   name: string;
-  price: number;
-  macros:{
-      protein: number;
-      carbohydrate: number;
-      fat: number;
-      sugar: number;
-  }
+  price: string;
+  canteen: string;
+  picture: string;
+  macros: {
+    calories: number;
+    carbs: number;
+    protein: number;
+    fat: number;
+    sugar: number;
+  };
 }
 
-// Mongoose Schema for Food
 const FoodSchema: Schema = new Schema({
   name: { type: String, required: true },
-  price: { type: Number, required: true },
-  macros:{
-      protein: { type: Number, required: true },
-      carbohydrate: { type: Number, required: true },
-      fat: { type: Number, required: true },
-      sugar: { type: Number, required: true },
+  price: { type: String, required: true },
+  canteen: { type: String, required: true },
+  picture: { type: String },
+  macros: {
+    calories: { type: Number, required: true },
+    carbs: { type: Number, required: true },
+    protein: { type: Number, required: true },
+    fat: { type: Number, required: true },
+    sugar: { type: Number, required: true },
   }
+}, {
+  timestamps: true,
 });
-
-export default mongoose.model<IFood>('Food', FoodSchema);
