1c21a7ca18d531a9d562c8118d7faa82934ff445
SupanutMT <supanut27530@gmail.com>
2026-03-01 15:17:24 +0700

    (feat) Add Food.ts

--- server/src/models/Food.ts
+++ server/src/models/Food.ts
@@ -1,1 +1,31 @@
 import mongoose, { Document, Schema } from 'mongoose';
+
+export interface IUser extends Document{
+  name: string;
+  price: string;
+  canteen: string;
+  picture: string;
+  macros: {
+    calories: number;
+    carbs: number;
+    protein: number;
+    fat: number;
+    sugar: number;
+  };
+}
+
+const FoodSchema: Schema = new Schema({
+  name: { type: String, required: true },
+  price: { type: String, required: true },
+  canteen: { type: String, required: true },
+  picture: { type: String },
+  macros: {
+    calories: { type: Number, required: true },
+    carbs: { type: Number, required: true },
+    protein: { type: Number, required: true },
+    fat: { type: Number, required: true },
+    sugar: { type: Number, required: true },
+  }
+}, {
+  timestamps: true,
+});
